e9c38f8b58f480ddde4eff17d44e4db1ebcaf3d1
pavel4ai <pavel@centml.ai>
2025-06-23 14:38:12 -0400

    changing model download logic

--- download_and_verify_weights.sh
+++ download_and_verify_weights.sh
@@ -9,24 +9,26 @@ RETRY_DELAY=10  # seconds
 DOWNLOAD_SUCCESS=false # Flag to track success
 WEIGHTS_DIR="/workspace/Wan2.1/Wan2.1-I2V-14B-720P"
 
-for ((i=1; i<=MAX_RETRIES; i++)); do
-    echo "Attempt $i of $MAX_RETRIES..."
-    huggingface-cli download "$MODEL_NAME" --local-dir ${WEIGHTS_DIR} --resume-download
-    if [ $? -eq 0 ]; then
-        echo "Download completed successfully."
-        DOWNLOAD_SUCCESS=true
-        break # Exit the loop on success
-    else
-        echo "Download failed on attempt $i. Retrying in $RETRY_DELAY seconds..."
-        sleep $RETRY_DELAY
-    fi
-done
+huggingface-cli download "$MODEL_NAME" --local-dir ${WEIGHTS_DIR} --resume-download
+
+#for ((i=1; i<=MAX_RETRIES; i++)); do
+#    echo "Attempt $i of $MAX_RETRIES..."
+#    huggingface-cli download "$MODEL_NAME" --local-dir ${WEIGHTS_DIR} --resume-download
+#    if [ $? -eq 0 ]; then
+#        echo "Download completed successfully."
+#        DOWNLOAD_SUCCESS=true
+#        break # Exit the loop on success
+#    else
+#        echo "Download failed on attempt $i. Retrying in $RETRY_DELAY seconds..."
+#        sleep $RETRY_DELAY
+#    fi
+#done
 
 # Check if download ultimately failed
-if [ "$DOWNLOAD_SUCCESS" = false ]; then
-    echo "Download failed after $MAX_RETRIES attempts."
-    exit 1
-fi
+#if [ "$DOWNLOAD_SUCCESS" = false ]; then
+#    echo "Download failed after $MAX_RETRIES attempts."
+#    exit 1
+#fi
 
 
 
